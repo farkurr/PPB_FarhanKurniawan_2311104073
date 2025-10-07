d52cec81b0ac7e0f1ccea882ab2f69c70790b0c1
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-10-07 15:44:11 +0700

    menambahkan praktikum modul 3

---
+++ 03_Pengenalan_Dart/Praktikum/variable.dart
@@ -0,0 +1,9 @@
+void main() {
+  // var name = "farhan";
+  // var age = 21;
+  // print("Nama: $name, umur : $age");
+
+  // String name = "Ilham";
+  // int age = 22;
+  // print("Nama: $name, umur : $age");
+}

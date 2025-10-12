8da04733e98499017b377024686a9684e48c8c57
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-10-12 18:06:24 +0700

    menambahkan guided

---
+++ 04_Antarmuka_Pengguna/Guided/guided_4/android/app/src/main/kotlin/com/example/guided_4/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.guided_4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ 04_Antarmuka_Pengguna/Guided/guided_4/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(color: Colors.red, height: 300, width: 300),
+          Container(color: Colors.white, height: 280, width: 280),
+          Container(color: Colors.blue, height: 260, width: 260),
+        ],
+      ),
+    );
+  }
+}

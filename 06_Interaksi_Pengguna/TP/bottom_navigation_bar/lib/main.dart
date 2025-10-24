0aee1b138dd5eb03017133232c2008082637830a
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-10-24 08:05:46 +0700

    menambahkan tp 6 dan laporannya

---
+++ 06_Interaksi_Pengguna/TP/bottom_navigation_bar/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Latihan BottomNavigationBar',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: const BottomNavExample(),
+    );
+  }
+}
+
+class BottomNavExample extends StatefulWidget {
+  const BottomNavExample({super.key});
+
+  @override
+  State<BottomNavExample> createState() => _BottomNavExampleState();
+}
+
+class _BottomNavExampleState extends State<BottomNavExample> {
+  int _selectedIndex = 0;
+
+  // daftar halaman yang ditampilkan sesuai menu
+  static const List<Widget> _pages = <Widget>[
+    Center(
+      child: Text('Ini adalah halaman Home', style: TextStyle(fontSize: 18)),
+    ),
+    Center(
+      child: Text('Ini adalah halaman Wisata', style: TextStyle(fontSize: 18)),
+    ),
+    Center(
+      child: Text('Ini adalah halaman Profil', style: TextStyle(fontSize: 18)),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Latihan BottomNavigationBar'),
+        backgroundColor: Colors.orange,
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            label: 'Wisata',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.orange,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
